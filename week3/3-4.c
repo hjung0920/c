60a9944503add49e6c943209c3180027b718b162
hjung0920 <likekhj@sookmyung.ac.kr>
2024-09-28 02:24:33 +0900

    3-4

---
+++ week3/3-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	float a,b;
+
+	
+	printf("enter two ingers:");
+	scanf("%f, %f", &a, &b);
+	
+	
+	printf("%6f / %6f = %6f\n", a,b,a/b );
+	
+	
+	return 0;
+}
